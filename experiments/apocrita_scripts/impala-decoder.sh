21380c3e9d3ff5215a37ec395bb2a6b416153fe1
Bam4d <chrisbam4d@gmail.com>
2021-02-26 10:23:31 +0000

    adding more bit of script

--- experiments/apocrita_scripts/impala-decoder.sh
+++ experiments/apocrita_scripts/impala-decoder.sh
@@ -8,16 +8,18 @@
 
 
 # Use the requested GPU
-export WANDB_API_KEY=684b0eeb870101c930baca51c6cc3b024d12147a
+export WANDB_API_KEY=$(cat key.txt)
 
 module load anaconda3
 module load cuda
 module load java/11.0.2
-module load ffmpeg/4.1.6-singularity
+module load use.dev ffmpeg/4.1.6-singularity
 
 conda create --name microrts python=3.7
 conda activate microrts
 
 pip install -r requirements.txt
+cd ../../ && pip install -e .
+
 
 python ../paper/ppo_gridnet_diverse_impala_decoder.py  --num-bot-envs 24 --cuda True --wandb-project-name gridnet  --wandb-entity chrisbam4d --prod-mode --total-timesteps 1000000
